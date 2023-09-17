dc79cc9001a5d8425f130ccd9ec2e3aff097aa39
Cosmic-rare <ysolakey1116@gmail.com>
2023-09-17 15:36:43 +0900

    feat: 🎸 ENVからnginx.confのstreamを生成

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-cat /usr/share/nginx/html/index.html
+cat > /etc/nginx/nginx.conf << EOL
+user  nginx;
+worker_processes  auto;
+
+error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+stream {
+    server {
+        listen ${PORT};
+        proxy_pass ${SERVER};
+    }
+}
+EOL
 
 nginx -g "daemon off;"
